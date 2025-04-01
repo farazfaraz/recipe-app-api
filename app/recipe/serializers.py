1247450c27cefab07afe54afdc8fbee63470acb3
faraz <rezaei.abdolhadi@gmail.com>
2025-04-01 14:55:54 +0200

    Implement create ingredients feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -28,9 +28,10 @@ class TagSerializer(serializers.ModelSerializer):
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
     tags = TagSerializer(many=True, required=False) # by default nested serializers are read_only, so we have to override the create functionality
+    ingredients = IngredientSerializer(many=True, required=False)
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
+        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags', 'ingredients']
         read_only_fields = ['id'] # meaning it cannot be changed by users.
 
     def _get_or_create_tag(self, tags, recipe):
@@ -44,11 +45,23 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
             recipe.tags.add(tag_obj) # Add the tag to the recipe
 
+    def _get_or_create_ingredient(self, ingredients, recipe):
+        """Handle getting or creating ingredients as needed."""
+        auth_user = self.context['request'].user
+        for ingredient in ingredients:
+            ingredient_obj, created = Ingredient.objects.get_or_create(
+                user=auth_user,
+                **ingredient,
+            )
+            recipe.ingredients.add(ingredient_obj)
+
     def create(self, validated_data):
         """Create a recipe."""
         tags = validated_data.pop('tags', []) # Extract tags from request data
+        ingredients = validated_data.pop('ingredients', [])
         recipe = Recipe.objects.create(**validated_data) # Create the recipe
         self._get_or_create_tag(tags, recipe)
+        self._get_or_create_ingredient(ingredients, recipe)
 
 
         return recipe
